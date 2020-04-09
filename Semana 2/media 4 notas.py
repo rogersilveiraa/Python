37c9ef3dac24c9c0561eb36918169acf37ed8e75
Roger Silveira <roger.silveira95@hotmail.com>
2020-04-09 16:44:56 -0300

    Create media 4 notas.py

---
+++ Semana 2/media 4 notas.py
@@ -0,0 +1,8 @@
+a = float(input("Digite a primeira nota:"))
+b = float(input("Digite a segunda nota:"))
+c = float(input("Digite a terceira nota:"))
+d = float(input("Digite a quarta nota:"))
+
+m = (a+b+c+d) / 4
+
+print ("A média aritmética é", m)
