c5de3eb1c44d2fe7a99560fb0de6a0182711939a
Roger Silveira <roger.silveira95@hotmail.com>
2020-04-09 16:48:52 -0300

    Create Teste de vogal.py

---
+++ Semana 5/Teste de vogal.py
@@ -0,0 +1,9 @@
+def vogal (x):
+
+    teste = False
+    
+    
+    if x == "a" or x == "e" or x == "i" or x == "o" or x == "u" or x == "A" or x == "E" or x == "I" or x == "O" or x == "U":
+        teste = True
+
+    return teste
