3f7866fa72f07b7cb1d0d6ed1a21d4ff7b325b01
Roger Silveira <roger.silveira95@hotmail.com>
2020-04-09 16:44:01 -0300

    Delete ordem crescente.py

--- ordem crescente.py
+++
@@ -1,9 +0,0 @@
-x = int(input("Digite o primeiro número:"))
-y = int(input("Digite o segundo número:"))
-z = int(input("Digite o terceiro número:"))
-
-if x < y < z:
-    print ("crescente")
-
-else:
-    print ("não está em order crescente")
