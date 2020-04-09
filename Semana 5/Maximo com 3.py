50a19201f67ec255b4d22e3803b32bddd5c24763
Roger Silveira <roger.silveira95@hotmail.com>
2020-04-09 16:48:49 -0300

    Create Maximo com 3.py

---
+++ Semana 5/Maximo com 3.py
@@ -0,0 +1,12 @@
+def maximo (x ,y ,z):
+
+    if x >= y:
+        if x >= z:
+            return x
+        else:
+            return z
+    else:
+        if y >= z:
+            return y
+        else:
+            return z 
